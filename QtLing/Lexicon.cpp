77e80497efa98fa47944d25cc115852fbbda332c
Tapan Srivastava <tapansriv@gmail.com>
2017-08-10 12:28:13 -0700

    made Lexicon members pointers and organized class

--- QtLing/Lexicon.cpp
+++ QtLing/Lexicon.cpp
@@ -1,11 +1,11 @@
 #include "Lexicon.h"
 
-//#include "SignatureCollection.h"
-//#include "StemCollection.h"
-//#include "SuffixCollection.h"
-//#include "WordCollection.h"
-//#include "StringSurrogate.h"
-#include "Word.h"
+#include "SignatureCollection.h"
+#include "StemCollection.h"
+#include "SuffixCollection.h"
+#include "WordCollection.h"
+#include "StringSurrogate.h"
+
 CLexicon::CLexicon() : m_Words(), m_Stems(), m_Suffixes(), m_Signatures()
 {
     m_Protostems = QMap<CStringSurrogate, int>();
--- QtLing/Lexicon.h
+++ QtLing/Lexicon.h
@@ -5,34 +5,34 @@
 #include <QString>
 
 
-#include "SignatureCollection.h"
-#include "StemCollection.h"
-#include "SuffixCollection.h"
-#include "WordCollection.h"
-#include "StringSurrogate.h"
+class CWordCollection;
+class CStemCollection;
+class CSuffixCollection;
+class CSignatureCollection;
+class CStringSurrogate;
 
 class CLexicon
 {
 protected:
-//    CWordCollection m_Words;
-    CStemCollection m_Stems;
-    CSuffixCollection m_Suffixes;
-//    CSignatureCollection m_Signatures;
+    CWordCollection *m_Words;
+    CStemCollection *m_Stems;
+    CSuffixCollection *m_Suffixes;
+    CSignatureCollection *m_Signatures;
 public:
     CLexicon();
 public:
-    // accessors
+    // accessors and protostems
     QMap<CStringSurrogate, int> m_Protostems;
-    CWordCollection m_Words;
-    CSignatureCollection m_Signatures;
 
+    CWordCollection*      GetWordCollection() { return m_Words; }
+    CStemCollection*      GetStemCollection() { return m_Stems; }
+    CSuffixCollection*    GetSuffixCollection() { return m_Suffixes; }
+    CSignatureCollection* GetSignatureCollection()     { return m_Signatures; }
+public:
+    // insert functions here
     void FindProtostems();
     void CreateStemAffixPairs();
     void AssignSuffixesToStems();
-
-    CWordCollection      GetWordCollection() { return m_Words; }
-    CSignatureCollection GetSignatures()     { return m_Signatures; }
-
 };
 
 #endif // CLEXICON_H
