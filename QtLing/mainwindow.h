ca55ed73c1362a4b8a90b25c5dea66aa79de71d2
Tapan Srivastava <tapansriv@gmail.com>
2017-08-27 10:01:56 -0700

    revert to pre-lexicon functions

--- QtLing/mainwindow.h
+++ QtLing/mainwindow.h
@@ -5,6 +5,7 @@
 
 #include <QMap>
 #include <QList>
+#include "Lexicon.h"
 
 QT_BEGIN_NAMESPACE
 class QAction;
@@ -18,21 +19,16 @@ class QPushButton;
 class QPlainTextEdit;
 class QTextEdit;
 class QSessionManager;
-class QTreeView;
 QT_END_NAMESPACE
-
-#include "Lexicon.h"
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    CLexicon* lexicon;
-
-    // to be deleted
-    QMap<QString, int> Words;
+    CLexicon *lexicon;
+    QMap<QString, int> Words; //would correspond to word collection
     QMap<QString, int> Protostems;
     QList<QPair<QString,QString>> Parses;
-    QMap<QString,QMap<QString,int>> Stems;
+    QMap<QString,QMap<QString,int>> Stems; // value is a map from suffixes so that it acts like a set.
     QMap<QString,QStringList> Signatures;
 public:
     MainWindow();
@@ -71,8 +67,86 @@ private:
     QGroupBox *verticalGroupBox;
     QPlainTextEdit *textEdit;
     QPlainTextEdit *littleEditor;
-    QTreeView      *treeView;
     QString curFile;
 };
 
 #endif
+
+//#ifndef MAINWINDOW_H
+//#define MAINWINDOW_H
+
+//#include <QMainWindow>
+
+//#include <QMap>
+//#include <QList>
+
+//QT_BEGIN_NAMESPACE
+//class QAction;
+//class QDialogButtonBox;
+//class QGroupBox;
+//class QLabel;
+//class QLineEdit;
+//class QMenu;
+//class QMenuBar;
+//class QPushButton;
+//class QPlainTextEdit;
+//class QTextEdit;
+//class QSessionManager;
+//class QTreeView;
+//QT_END_NAMESPACE
+
+//#include "Lexicon.h"
+
+//class MainWindow : public QMainWindow
+//{
+//    Q_OBJECT
+//    CLexicon* lexicon;
+
+//    // to be deleted
+//    QMap<QString, int> Words;
+//    QMap<QString, int> Protostems;
+//    QList<QPair<QString,QString>> Parses;
+//    QMap<QString,QMap<QString,int>> Stems;
+//    QMap<QString,QStringList> Signatures;
+//public:
+//    MainWindow();
+
+//    void loadFile(const QString &fileName);
+
+//protected:
+//    void closeEvent(QCloseEvent *event) override;
+//    void FindProtostems();
+//    void CreateStemAffixPairs();
+//    void AssignSuffixesToStems();
+//private slots:
+//    void newFile();
+//    void open();
+//    bool save();
+//    bool saveAs();
+//    void about();
+//    void documentWasModified();
+//#ifndef QT_NO_SESSIONMANAGER
+//    void commitData(QSessionManager &);
+//#endif
+
+//private:
+//    void createHorizontalGroupBox();
+
+//    void createActions();
+//    void createStatusBar();
+//    void readSettings();
+//    void writeSettings();
+//    bool maybeSave();
+//    bool saveFile(const QString &fileName);
+//    void setCurrentFile(const QString &fileName);
+//    QString strippedName(const QString &fullFileName);
+
+//    QGroupBox *horizontalGroupBox;
+//    QGroupBox *verticalGroupBox;
+//    QPlainTextEdit *textEdit;
+//    QPlainTextEdit *littleEditor;
+//    QTreeView      *treeView;
+//    QString curFile;
+//};
+
+//#endif
