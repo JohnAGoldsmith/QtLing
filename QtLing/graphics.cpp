05ce7836355c760a7e374086e60d25b7b8fd9b20
John A Goldsmith <goldsmith@uchicago.edu>
2017-11-06 20:29:18 -0600

    Good graphics for signatures -- Control G

---
+++ QtLing/graphics.cpp
@@ -0,0 +1,68 @@
+#include <QDebug>
+#include "graphics.h"
+#include "generaldefinitions.h"
+#include "SignatureCollection.h";
+#include "mainwindow.h"
+
+lxa_graphics_view::lxa_graphics_view(MainWindow* this_window, lxa_graphics_scene* my_scene)
+{
+    m_main_window = this_window;
+    m_graphics_scene = my_scene;
+
+    setScene(m_graphics_scene);
+
+};
+
+lxa_graphics_scene::lxa_graphics_scene (MainWindow * window)
+{
+    m_main_window = window;
+
+    //setBackgroundBrush(Qt::yellow);
+};
+
+void lxa_graphics_scene::set_graphics_view(lxa_graphics_view * this_graphics_view)
+{
+    m_graphics_view  = this_graphics_view;
+}
+void lxa_graphics_scene::ingest_signatures(CSignatureCollection* signatures){
+    qDebug() << "Ingesting signatures.";
+    int max_size = 0;
+    int sig_size;
+    CSignature * pSig;
+    map_sigstring_to_sig_ptr_iter sig_iter(*signatures->get_map());
+    while(sig_iter.hasNext()){
+        int this_size = sig_iter.next().value()->get_number_of_affixes();
+        if (this_size > max_size){ max_size = this_size;}
+    }
+    for (int size = 0; size <= max_size; size++){
+        QList<CSignature*> * signature_list = new QList<CSignature*>;
+        m_signature_lattice.append(signature_list);
+    }
+    sig_iter.toFront();
+    while(sig_iter.hasNext()){
+        pSig = sig_iter.next().value();
+        sig_size = pSig->get_number_of_affixes();
+        m_signature_lattice[sig_size]->append(pSig);
+        //qDebug() << pSig->get_key();
+    }
+}
+void lxa_graphics_scene::place_signatures()
+{
+    int row_delta = 60;
+    int col_delta = 30;
+    int number_of_rows = m_signature_lattice.size();
+    int bottom_row = row_delta * number_of_rows;
+    int sig_radius = 20;
+
+    for (int row = 2; row <m_signature_lattice.size(); row++){
+        CSignature_ptr_list_iterator sig_iter(*m_signature_lattice[row]);
+        int col = 0;
+        while (sig_iter.hasNext()){
+            CSignature* pSig = sig_iter.next();
+            int x = col * col_delta;
+            int y = bottom_row - (row-2) * row_delta;
+            addEllipse(x,y,sig_radius,sig_radius,QPen(),QBrush(Qt::red));
+            col++;
+        }
+    }
+}
---
+++ QtLing/graphics.h
@@ -0,0 +1,47 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include<QGraphicsItem>
+#include <QGraphicsView>
+
+class CSignature;
+class CSignatureCollection;
+class MainWindow;
+class lxa_graphics_scene;
+
+class lxa_graphics_view : public QGraphicsView
+{
+    QList<QList<CSignature*>*>  m_signature_lattice;
+    MainWindow *                m_main_window;
+    QGraphicsScene*              m_graphics_scene;
+public:
+    lxa_graphics_view( MainWindow * , lxa_graphics_scene * my_scene);
+    void draw_signatures();
+
+};
+
+class lxa_graphics_scene : public QGraphicsScene
+{
+    MainWindow*                     m_main_window;
+    lxa_graphics_view*              m_graphics_view;
+    QList<QList<CSignature*>*>      m_signature_lattice;
+
+public:
+                    lxa_graphics_scene( MainWindow * );
+    void            set_graphics_view (lxa_graphics_view* );
+    void            ingest_signatures(CSignatureCollection*);
+    void            place_signatures();
+};
+
+class signature_node : public QGraphicsItem
+{
+public:
+    QRectF boundingRect() const
+    {
+
+    }
+    void paint (QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget){
+        painter->drawEllipse(100,100,10,10);
+    }
+};
+
+#endif // GRAPHICS_H
