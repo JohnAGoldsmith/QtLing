81caab7e597b136c2f73cf31cfc3d810697e600b
Hanson Lu <hansonlu@uchicago.edu>
2018-07-17 17:06:15 -0500

    suffix and prefix to json

--- QtLing/Suffix.cpp
+++ QtLing/Suffix.cpp
@@ -11,6 +11,12 @@ CSuffix::CSuffix(CSuffix& suffix)
     m_count = 0;
 }
 
+void CSuffix::to_json(QJsonObject &ref_json)
+{
+    ref_json["suffix"] = m_key;
+    ref_json["frequency"] = m_frequency;
+    ref_json["count"] = m_count;
+}
 
 CPrefix::CPrefix(QString  suffix): m_key(suffix)
 {m_count = 0;
@@ -21,4 +27,11 @@ CPrefix::CPrefix(CPrefix& suffix)
     m_key = suffix.get_key();
     m_frequency = suffix.GetFrequency();
     m_count = 0;
+}
+
+void CPrefix::to_json(QJsonObject &ref_json)
+{
+    ref_json["prefix"] = m_key;
+    ref_json["frequency"] = m_frequency;
+    ref_json["count"] = m_count;
 }
--- QtLing/Suffix.h
+++ QtLing/Suffix.h
@@ -1,51 +1,54 @@
 #ifndef CSUFFIX_H
 #define CSUFFIX_H
 
-    #include <QString>
-    #include <QChar>
-    #include <QList>
-    #include <QMap>
+#include <QString>
+#include <QChar>
+#include <QList>
+#include <QMap>
+#include <QJsonObject>
 
 
-    class CSuffix
-    {
-    protected:
-        QString m_key;
-        int m_frequency;
-        int m_count;
-    public:
-        CSuffix(QString ssWord);
-        CSuffix(CSuffix&);
-    public:
-        //Accessors
-        QString get_key() const                 {return m_key;}
-        QString GetSuffix() const               { return m_key;    }
-        int     GetFrequency()                  { return m_frequency; }
-        void    SetFrequency(int frequency)     { m_frequency = frequency; }
-        int     get_count() const               { return m_count;}
-        int     increment_count()               { m_count+= 1; return m_count;}
-    };
+class CSuffix
+{
+protected:
+    QString m_key;
+    int m_frequency;
+    int m_count;
+public:
+    CSuffix(QString ssWord);
+    CSuffix(CSuffix&);
+public:
+    //Accessors
+    QString get_key() const                 {return m_key;}
+    QString GetSuffix() const               { return m_key;    }
+    int     GetFrequency()                  { return m_frequency; }
+    void    SetFrequency(int frequency)     { m_frequency = frequency; }
+    int     get_count() const               { return m_count;}
+    int     increment_count()               { m_count+= 1; return m_count;}
+    void    to_json(QJsonObject& ref_json);
+};
 
-    class CPrefix
-    {
-    protected:
-        QString m_key;
-        int m_frequency;
-        int m_count;
-    public:
-        CPrefix(QString ssWord);
-        CPrefix(CPrefix&);
-    public:
-        //Accessors
-        QString get_key() const                 { return m_key;}
-        QString GetPrefix() const               { return m_key;    }
-        int     GetFrequency()                  { return m_frequency; }
-        void    SetFrequency(int frequency)     { m_frequency = frequency; }
-        int     get_count() const               { return m_count;}
-        int     increment_count()               { m_count+= 1; return m_count;}
-    };
+class CPrefix
+{
+protected:
+    QString m_key;
+    int m_frequency;
+    int m_count;
+public:
+    CPrefix(QString ssWord);
+    CPrefix(CPrefix&);
+public:
+    //Accessors
+    QString get_key() const                 { return m_key;}
+    QString GetPrefix() const               { return m_key;    }
+    int     GetFrequency()                  { return m_frequency; }
+    void    SetFrequency(int frequency)     { m_frequency = frequency; }
+    int     get_count() const               { return m_count;}
+    int     increment_count()               { m_count+= 1; return m_count;}
+    void    to_json(QJsonObject& ref_json);
+};
 
 
 
 
-    #endif // CSUFFIX_H
+#endif // CSUFFIX_H
